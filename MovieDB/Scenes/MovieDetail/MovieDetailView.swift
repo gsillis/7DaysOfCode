c80d3c2f2c5daa7e5272e94cccfe24c641ec3b41
Gabriela Sillis <gabydias47@gmail.com>
2023-02-20 13:31:09 -0300

    Changed: changes UIStackView constraints to fix layout issues

--- MovieDB/Scenes/MovieDetail/MovieDetailView.swift
+++ MovieDB/Scenes/MovieDetail/MovieDetailView.swift
@@ -28,20 +28,27 @@ final class MovieDetailView: UIView {
     
     private lazy var scrollView: UIScrollView = {
         let scroll = UIScrollView()
-        scroll.isScrollEnabled = true
+        scroll.alwaysBounceVertical = true
+        scroll.isUserInteractionEnabled = true
         return scroll
     }()
     
     private lazy var stackView: UIStackView = {
-        let stack = UIStackView(arrangedSubviews: [imageView, titleLabel, detailLabel])
+        let stack = UIStackView(arrangedSubviews: [titleLabel, detailLabel])
         stack.axis = .vertical
         stack.spacing = 20
         return stack
     }()
     
+    private lazy var contentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        return view
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupView()
+        buildView()
     }
     
     required init?(coder: NSCoder) {
@@ -54,20 +61,31 @@ extension MovieDetailView: ViewsProtocol {
         scrollView.snp.makeConstraints { make in
             make.top.bottom.leading.trailing.equalToSuperview()
         }
-       
-        stackView.snp.makeConstraints { make in
-            make.top.centerX.width.equalToSuperview()
-            make.bottom.equalTo(scrollView.snp.bottom)
-        }
         
         imageView.snp.makeConstraints { make in
-            make.height.equalToSuperview().offset(400)
+            make.top.equalTo(30)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(250)
+            make.width.equalTo(200)
+        }
+       
+        contentView.snp.makeConstraints { make in
+            make.top.edges.width.equalToSuperview()
+            make.height.greaterThanOrEqualTo(scrollView.snp.height)
+        }
+        
+        stackView.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(20)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
         }
     }
     
     func buildViewHierarchy() {
         addSubview(scrollView)
-        scrollView.addSubview(stackView)
+        scrollView.addSubview(contentView)
+        contentView.addSubview(imageView)
+        contentView.addSubview(stackView)
     }
     
     func setupView() {
@@ -79,6 +97,7 @@ extension MovieDetailView: DetailViewProtocol {
     func configure(with model: MovieModel) {
         titleLabel.text = model.title
         detailLabel.text = model.overview
+        imageView.loadImage(from: model.posterPathImage)
     }
 }
 
@@ -89,6 +108,23 @@ struct DetailPreview: PreviewProvider {
     static var previews: some View {
         Preview {
             let view = MovieDetailView()
+            let model = MovieModel(
+                posterPath: "any",
+                adult: false,
+                overview: "any",
+                releaseDate: "any",
+                genreIDS: [1],
+                id: 0,
+                originalTitle: "any",
+                originalLanguage: "any",
+                title: "any",
+                backdropPath: "any",
+                popularity: 0.0,
+                voteCount: 1,
+                video: false,
+                voteAverage: 0.0
+            )
+            view.configure(with: model)
             return view
         }
         .previewLayout(
