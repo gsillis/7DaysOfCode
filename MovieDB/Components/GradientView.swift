66b5bc268e3444c796ddd7992c4859cd33057fa4
Gabriela Sillis <gabydias47@gmail.com>
2023-02-21 18:55:19 -0300

    Added: creates gradient view

---
+++ MovieDB/Components/GradientView.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+final class GradientView: UIView {
+    private var gradientLayer = CAGradientLayer()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupGradient()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = bounds
+    }
+    
+    private func setupGradient() {
+        gradientLayer.colors = [
+            UIColor.customPurple.cgColor,
+            UIColor.customPurpleDark.cgColor
+        ]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
+
